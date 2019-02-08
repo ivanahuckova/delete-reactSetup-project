6f29d69cf6ff56b0653b30240b460c499ee83a4b
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-08 13:41:11 +0100

    finish babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  presets: [['@babel/preset-env'], ['@babel/preset-react']],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          chrome: '60',
+        },
+      },
+    ],
+    ['@babel/preset-react'],
+  ],
 };
